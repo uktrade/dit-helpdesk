fb19159dde0b8d91643b019eeee6f5c6078af0c2
Ross Rochford <digiology@gmail.com>
2019-01-30 16:43:36 +0000

    remove everything

--- requirements_documents/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RequirementsDocumentsConfig(AppConfig):
-    name = 'requirements_documents'
--- requirements_documents/management/commands/get_eu_documents_brens_solution.py
+++
@@ -1,4 +0,0 @@
-
-
-REQUIREMENTS_URL = 'https://trade.ec.europa.eu/services/reqs/public/v1/requirements?lang=EN&originCountry=%s&destinationCountry=GB&product=%s'
-
