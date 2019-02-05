d4c3948cae5a505658cacd84783e901b12d4d3d6
Ian James <ian.james@digital.cabinet-office.gov.uk>
2019-02-05 14:59:56 +0000

    Rebase.

---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+'use strict'
+
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+// const rev = require('gulp-rev')
+const sourcemaps = require('gulp-sourcemaps')
+const cssnano = require('gulp-cssnano')
+
+sass.compiler = require('node-sass')
+
+// const environment = process.env.NODE_ENV || 'dev'
+
+const paths = {
+  styles: {
+    folder: './assets/',
+    source: './assets/global.scss',
+    destination: './dit_helpdesk/static/css/'
+  },
+  govukFrontendAssets: {
+    source: './node_modules/govuk-frontend/assets/**/*.*',
+    destination: './dit_helpdesk/static/'
+  },
+  manifest: './manifest'
+}
+
+gulp.task('sass', () => {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'))
+})
+
+gulp.task('sass:watch', () => {
+  gulp.watch('./sass/**/*.scss', ['sass'])
+})
+
+const styles = () => {
+  return gulp.src(paths.styles.source)
+    .pipe(sass({
+      includePaths: 'node_modules'
+    }))
+    .pipe(sourcemaps.init())
+    // .pipe(rev())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cssnano())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(paths.styles.destination))
+    // .pipe(rev.manifest({ merge: true }))
+    // .pipe(gulp.dest(paths.manifest))
+}
+
+const copyAssets = () => {
+  return gulp.src(paths.govukFrontendAssets.source)
+    .pipe(gulp.dest(paths.govukFrontendAssets.destination))
+}
+
+const watch = () => {
+  gulp.watch(paths.styles.folder, styles)
+}
+
+gulp.task('default', watch)
+gulp.task('styles', styles)
+gulp.task('copy', copyAssets)
