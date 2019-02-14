39d4342086f371a84587fc830517fa2529c04a66
Ross Rochford <digiology@gmail.com>
2019-02-14 13:47:14 +0000

    fixed settings for production

--- dit_helpdesk/dit_helpdesk/settings/base.py
+++ dit_helpdesk/dit_helpdesk/settings/base.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nz$5_3@0snh+p3q9dq_w55f-#ogxtlx3w9w!lej5br(7k$80r%'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- dit_helpdesk/dit_helpdesk/settings/prod.py
+++ dit_helpdesk/dit_helpdesk/settings/prod.py
@@ -1,3 +1,5 @@
+from base import *
+
 import dj_database_url
 
 
