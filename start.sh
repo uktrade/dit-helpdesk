01f3c411d6f1db6d7f6c066a1adf9273655b9fd7
Marcus Patino Pan <marcus.patino-pan@digital.trade.gov.uk>
2019-02-22 11:29:13 +0000

    Dockerized

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -xe
+gulp build:all
+python dit_helpdesk/manage.py collectstatic --noinput
+python dit_helpdesk/manage.py migrate
+python dit_helpdesk/manage.py create_admin_user
+python dit_helpdesk/manage.py loaddata countries_data
+python dit_helpdesk/manage.py runserver_plus 0.0.0.0:8000
+
+echo "docker exec -it dit-helpdesk_helpdesk_1 /bin/bash"
+echo "python dit_helpdesk/manage.py scrape_section_hierarchy 1"
