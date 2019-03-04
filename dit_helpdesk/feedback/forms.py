4e983b0280f13e1c91986a29f9532255aa54ac47
Ian James <ian.james@digital.cabinet-office.gov.uk>
2019-03-04 12:33:37 +0000

    Error messages for email (syntax) and message (required) set up.

--- dit_helpdesk/feedback/forms.py
+++ dit_helpdesk/feedback/forms.py
@@ -6,4 +6,13 @@ class FeedbackForm(forms.ModelForm):
 
     class Meta:
         model = Feedback
-        fields = ['name', 'email', 'message']
+        fields = ['message', 'name', 'email']
+        error_messages = {
+            'message': {
+                'required': "Enter a message",
+                'limit': "The message needs to be less than 1000 characters"
+            },
+            'email': {
+                'invalid' : 'Enter a valid email address'
+            }
+        }
--- dit_helpdesk/feedback/models.py
+++ dit_helpdesk/feedback/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Feedback(models.Model):
-    name = models.CharField(max_length=255)
-    email = models.EmailField()
     message = models.TextField()
+    name = models.CharField(max_length=255, blank=True)
+    email = models.EmailField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
