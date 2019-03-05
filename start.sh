eff82a417ac724ba3362900a8dbb8da1e2b110a3
Ian James <1732331+injms@users.noreply.github.com>
2019-03-05 14:05:27 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,8 +6,8 @@ python dit_helpdesk/manage.py loaddata countries_data
 # npm install
 # npm rebuild node-sass
 # npm run build
-sleep infinity
-# python dit_helpdesk/manage.py runserver_plus 0.0.0.0:8000
+# sleep infinity
+python dit_helpdesk/manage.py runserver_plus 0.0.0.0:8000
 
 echo "docker exec -it dit-helpdesk_helpdesk_1 /bin/bash"
 echo "python dit_helpdesk/manage.py scrape_section_hierarchy 1"
