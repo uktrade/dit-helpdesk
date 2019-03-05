3794aaf55786790699b0c63414bfebfd4e2f5d41
Ian James <ian.james@digital.cabinet-office.gov.uk>
2019-03-05 11:00:10 +0000

    Adds length check error messages for name.

--- dit_helpdesk/feedback/forms.py
+++ dit_helpdesk/feedback/forms.py
@@ -9,8 +9,11 @@ class FeedbackForm(forms.ModelForm):
         fields = ['message', 'name', 'email']
         error_messages = {
             'message': {
-                'required': "Enter a message",
-                'max_length': "The message needs to be less than 1,000 characters"
+                'required': 'Enter a message',
+                'max_length': 'Message needs to be less than 1,000 characters'
+            },
+            'name': {
+                'max_length': 'Name entered needs to be less than 255 characters'
             },
             'email': {
                 'invalid' : 'Enter a valid email address'
