8817fe012c97dbcf53ac630e4afc3c8f92314ece
Ian James <1732331+injms@users.noreply.github.com>
2019-03-08 17:19:27 +0000

    Form displays errors (#21)

--- dit_helpdesk/feedback/forms.py
+++ dit_helpdesk/feedback/forms.py
@@ -6,4 +6,16 @@ class FeedbackForm(forms.ModelForm):
 
     class Meta:
         model = Feedback
-        fields = ['name', 'email', 'message']
+        fields = ['message', 'name', 'email']
+        error_messages = {
+            'message': {
+                'required': 'Enter a message',
+                'max_length': 'Message needs to be less than 1,000 characters'
+            },
+            'name': {
+                'max_length': 'Name entered needs to be less than 255 characters'
+            },
+            'email': {
+                'invalid' : 'Enter a valid email address'
+            }
+        }
