031c5d2bb4c0881e5cfd197d9fdaea0e8692034c
Ian James <ian.james@digital.cabinet-office.gov.uk>
2019-03-22 11:18:42 +0000

    Removed unnecessary static files.

--- dit_helpdesk/static/js/enhancements.js
+++
@@ -1,7 +0,0 @@
-import ScrollingTable from './modules/scrolling-tables.module.js'
-
-const $scrollingTables = document.querySelectorAll('[data-module="scrolling-table"]')
-
-$scrollingTables.forEach(($scrollingTable) => {
-  new ScrollingTable($scrollingTable).init()
-})
