77ee52c95010364eaf9ff0404e840623ac8e2118
kr8n3r <jani@kraner.dev>
2019-04-04 16:35:37 +0100

    Collapsable commodity tree javascript

--- assets/javascript/modules/commodity-tree.js
+++ assets/javascript/modules/commodity-tree.js
@@ -1,17 +1,33 @@
 require('govuk-frontend/vendor/polyfills/Function/prototype/bind')
-require('govuk-frontend/vendor/polyfills/Element/prototype/classList')
 require('govuk-frontend/vendor/polyfills/Event')
+require('../vendor/polyfills/array-filter')
 // enable collapsing on commodity tree
 var commodityTree = {
     init: function ($module) {
-        console.log($module)
-        /$module.addEventListener('click', commodityTree.handleClick.bind(this))
-        // commodityTree.handleClick()
+        $module.addEventListener('click', commodityTree.toggleSections.bind(this))
     },
-    collapseSection: function() {
-    },
-    handleClick: function(event) {
-        console.log('yes')
+    toggleSections: function(event) {
+        var $target = event.target
+        if ($target.tagName !== 'A') {
+            return false
+        }
+        
+        var $parentNode = $target.parentNode
+        if ($parentNode.className.indexOf('app-hierarchy-tree__parent--open') !== -1) {
+            event.preventDefault()
+            $parentNode.className = $parentNode.className.replace(/app-hierarchy-tree__parent--open/, 'app-hierarchy-tree__parent--closed js-closed')
+            var childList = Array.prototype.filter.call($parentNode.childNodes, function(el){
+                return el.className === 'app-hierarchy-tree--child';
+            });
+            childList[0].style.display = 'none';
+        } else if ($parentNode.className.indexOf('js-closed') !== -1) {
+            event.preventDefault()
+            $parentNode.className = $parentNode.className.replace(/app-hierarchy-tree__parent--closed js-closed/, 'app-hierarchy-tree__parent--open')
+            var childList = Array.prototype.filter.call($parentNode.childNodes, function(el){
+                return el.className === 'app-hierarchy-tree--child';
+            });
+            childList[0].style.display = 'block';
+        }
     }
 }
 module.exports = commodityTree
