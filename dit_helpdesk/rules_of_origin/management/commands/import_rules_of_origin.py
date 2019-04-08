7ce5268b70b48ab01878a39d81690880a1e29586
Glen Lavery <glen@lavery.co.uk>
2019-04-08 13:25:34 +0100

    added docx sraper class to managemnt and managemen command and modified mporter to accomodate new word doc content

--- dit_helpdesk/rules_of_origin/management/commands/import_rules_of_origin.py
+++ dit_helpdesk/rules_of_origin/management/commands/import_rules_of_origin.py
@@ -15,11 +15,22 @@ from rules_of_origin.importer import RulesOfOriginImporter
 class Command(BaseCommand):
 
     def add_arguments(self, parser):
-        parser.add_argument('--data_file', default=None)
+        parser.add_argument('--input_file', default=None)
+        parser.add_argument('--output_file', default=None)
+        parser.add_argument('--documents_file', default=None)
+        parser.add_argument('--groups_file', default=None)
+        parser.add_argument('--priority', type=int, nargs='?', default=1)
 
     def handle(self, *args, **options):
-        print("importer : ", options['data_file'])
-        if options['data_file'] is None:
+        opts = {option: options[option] for option in options if option.endswith('_file')}
+        print(opts)
+        print({'priority': options['priority']})
+        if None in opts.values():
+            print ("{0} is missing".format(list(opts.keys())[list(opts.values()).index(None)]))
             sys.exit()
         importer = RulesOfOriginImporter()
-        importer.load(data_file=options['data_file'])
+        importer.load(input_file=options['input_file'],
+                      output_file=options['output_file'],
+                      documents_file=options['documents_file'],
+                      groups_file=options['groups_file'],
+                      priority=options['priority'])
