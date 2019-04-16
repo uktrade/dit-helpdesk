6c90a614b7154d67686fa31fab8f1f55aa413028
Tom Bateman <t.r.bateman85@gmail.com>
2019-04-16 10:36:37 +0100

    Fixed broken test for model str method

--- dit_helpdesk/healthcheck/models.py
+++ dit_helpdesk/healthcheck/models.py
@@ -5,7 +5,7 @@ class HealthCheck(models.Model):
     health_check_field = models.BooleanField(null=False, default=True)
 
     def __unicode__(self):
-        return self.health_check_field
+        return "%s" % self.health_check_field
 
     def __str__(self):
-        return self.health_check_field
+        return "%s" % self.health_check_field
