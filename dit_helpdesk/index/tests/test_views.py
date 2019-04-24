c3df1b585fcc1140b31b5ee1850ddd9c3a59ce0b
Glen Lavery <glen@lavery.co.uk>
2019-04-24 15:11:49 +0100

    added tests for index app

---
+++ dit_helpdesk/index/tests/test_views.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+from django.urls import reverse
+
+
+class IndexViewTestCase(TestCase):
+
+    def test_index__returns_http_200(self):
+        resp = self.client.get(reverse('index'))
+        self.assertTrue(resp.status_code, 200)
