40d6713fac8526c276cdb8f8a2210a4cf592c759
Glen Lavery <glen@lavery.co.uk>
2019-04-25 03:36:54 +0100

    added test for commodities templatetags

---
+++ dit_helpdesk/commodities/tests/test_templatetags.py
@@ -0,0 +1,15 @@
+from django.template import Context, Template
+from django.test import TestCase
+
+from commodities.templatetags.cell_cleaner import cell_cleaner
+
+
+class TemplateTagsTestCase(TestCase):
+
+    def test_cell_cleaner_tag_filter_with_empty_value(self):
+        result = cell_cleaner('')
+        self.assertEqual(result, '-')
+
+    def test_cell_cleaner_tag_filter_with_non_empty_value(self):
+        result = cell_cleaner('my value')
+        self.assertEqual(result, 'my value')
