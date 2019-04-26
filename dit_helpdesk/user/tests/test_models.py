eb1938ed6e7365fb3a9d948ee83a66e9fde25d0e
Glen Lavery <glen@lavery.co.uk>
2019-04-26 05:35:16 +0100

    modified tests for user

--- dit_helpdesk/user/tests/test_models.py
+++ dit_helpdesk/user/tests/test_models.py
@@ -1,7 +1,16 @@
 from django.test import TestCase
+from mixer.backend.django import mixer
+
+from user.models import User
 
 
 class UserModelsTestCase(TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
 
+    def setUp(self):
+        self.user = mixer.blend(
+            User,
+            email="test@user.com"
+        )
+
+    def test_username_returns_email(self):
+        self.assertEqual(self.user.username, 'test@user.com')
