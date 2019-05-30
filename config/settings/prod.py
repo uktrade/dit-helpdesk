4946d23c07c7646504f951a9b89f2df65495df06
Tom Bateman <t.r.bateman85@gmail.com>
2019-05-30 12:12:01 +0100

    CF settings for elastic search and redis

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,3 +1,22 @@
 from .base import *
 
 ADMIN_ENABLED = False
+
+VCAP_SERVICES = os.environ.get('VCAP_SERVICES', {})
+
+ES_URL = VCAP_SERVICES['elasticsearch'][0]['credentials']['uri']
+
+ELASTICSEARCH_DSL = {
+    'default': {
+        'hosts': ES_URL
+    }
+}
+
+REDIS_URL = VCAP_SERVICES['redis'][0]['credentials']['uri']
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': REDIS_URL,
+    }
+}
