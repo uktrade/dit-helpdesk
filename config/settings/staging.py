53791c13a8bdc94d1092a0f9335963e960306121
Glen Lavery <glen@lavery.co.uk>
2019-08-08 23:08:52 +0100

    temporarily added debug_toolbar to staging to debug env issue

--- config/settings/staging.py
+++ config/settings/staging.py
@@ -21,3 +21,13 @@ CACHES = {
         'LOCATION': REDIS_URL,
     }
 }
+if DEBUG:
+    INSTALLED_APPS += ['debug_toolbar']
+    MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']
+
+    def show_toolbar(request):
+        return True
+
+    DEBUG_TOOLBAR_CONFIG = {
+        'SHOW_TOOLBAR_CALLBACK': show_toolbar,
+    }
