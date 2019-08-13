cc4f6092bf67db7f0590c9be502c1acee99fd91c
Glen Lavery <glen@lavery.co.uk>
2019-08-13 08:40:27 +0100

    remove unuswed search api references in urls.py

--- dit_helpdesk/search/urls.py
+++ dit_helpdesk/search/urls.py
@@ -1,20 +1,10 @@
 from django.urls import re_path
 
 from search import views
-from django.conf.urls import url, include
-from rest_framework.routers import SimpleRouter
 
 app_name = 'search'
 
-router = SimpleRouter()
-router.register(
-    prefix=r'',
-    basename='search',
-    viewset=views.CommodityViewSet
-)
-
 urlpatterns = [
-    re_path(r'^api/', include(router.urls)),
 
     re_path(
         r'country/(?P<country_code>\w+)/$',
