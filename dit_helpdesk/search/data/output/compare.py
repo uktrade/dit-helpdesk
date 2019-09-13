9e5b85e7f80904d1360c251d87eb820b986b863f
Glen Lavery <glen@lavery.co.uk>
2019-09-13 08:19:23 +0100

    removed commented code

--- dit_helpdesk/search/data/output/compare.py
+++ dit_helpdesk/search/data/output/compare.py
@@ -1,7 +1,3 @@
-# import pandas as pd
-#
-# one = pd.read_csv('commodity_category_all_with_synonyms_greenpage.csv')
-# two = pd.read_csv('keywords_and_synonyms_merged.csv')
 
 with open('commodity_category_all_with_synonyms_greenpage.csv', 'r') as file1:
     with open('keywords_and_synonyms_merged.csv', 'r') as file2:
