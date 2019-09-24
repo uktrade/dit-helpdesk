02db048fa1abe7826abd70638c16593b81b4a63e
Glen Lavery <glen@lavery.co.uk>
2019-09-24 01:59:22 +0100

    added comments relating to the index app

--- dit_helpdesk/index/views.py
+++ dit_helpdesk/index/views.py
@@ -4,7 +4,7 @@ from django.views.generic.base import RedirectView
 
 class IndexRedirect(RedirectView):
     """
-    Placeholder class based generic view redirecting to the DNS entry for the application
-
+    Placeholder class based generic view redirecting to the DNS entry for the application and the start page
+    requires setting the ENV variable bellow in the vault
     """
     url = "https://{0}/".format(settings.APP_START_DOMAIN)
