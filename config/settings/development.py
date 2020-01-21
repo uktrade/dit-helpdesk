19b81fdbbc4476f319262455b6313ba0e91fd81a
Tom Bateman <trbateman@users.noreply.github.com>
2020-01-21 14:50:17 +0000

    Settings name change (#142)
    * Seperated settings between development on a local machine, and development in Gov PaaS`
    
    * Updated template for new settins

--- config/settings/development.py
+++ config/settings/development.py
@@ -1,42 +1,23 @@
 from .base import *
+import json
 
-DEBUG = True
-RESTRICT_ADMIN = False
-DATABASES = {
-    "default": {
-        "ENGINE": "psqlextra.backend",  # 'django.db.backends.postgresql_psycopg2',
-        "NAME": os.environ.get("DJANGO_POSTGRES_DATABASE"),
-        "USER": os.environ.get("DJANGO_POSTGRES_USER"),
-        "PASSWORD": os.environ.get("DJANGO_POSTGRES_PASSWORD"),
-        "HOST": os.environ.get("DJANGO_POSTGRES_HOST"),
-        "PORT": os.environ.get("DJANGO_POSTGRES_PORT"),
-    }
-}
+VCAP_SERVICES = json.loads(os.environ.get("VCAP_SERVICES", {}))
 
-# Secure cookie settings.
-SESSION_COOKIE_SECURE = False
-SESSION_COOKIE_HTTPONLY = False
-CSRF_COOKIE_SECURE = False
-
-STATIC_ROOT = "dit_helpdesk/static"
-INTERNAL_IPS = ["127.0.0.1", "0.0.0.0", "localhost"]
-
-INSTALLED_APPS += ["debug_toolbar"]
-
-MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
-
-
-def show_toolbar(request):
-    return True
-
-
-DEBUG_TOOLBAR_CONFIG = {"SHOW_TOOLBAR_CALLBACK": show_toolbar}
-
-ES_URL = "http://es:9200"
+ES_URL = VCAP_SERVICES["elasticsearch"][0]["credentials"]["uri"]
 
 ELASTICSEARCH_DSL = {"default": {"hosts": ES_URL}}
 
-EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
-EMAIL_FILE_PATH = BASE_DIR + "/app-messages"
+REDIS_URL = VCAP_SERVICES["redis"][0]["credentials"]["uri"]
 
-FEEDBACK_DESTINATION_EMAIL = os.environ.get("FEEDBACK_DESTINATION_EMAIL")
+CACHES = {
+    "default": {"BACKEND": "django_redis.cache.RedisCache", "LOCATION": REDIS_URL}
+}
+
+if DEBUG:
+    INSTALLED_APPS += ["debug_toolbar"]
+    MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
+
+    def show_toolbar(request):
+        return True
+
+    DEBUG_TOOLBAR_CONFIG = {"SHOW_TOOLBAR_CALLBACK": show_toolbar}
