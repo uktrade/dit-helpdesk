2f840bfc8311547b6dbe6d1f8f6434567332c440
Glen Lavery <glavery@users.noreply.github.com>
2020-01-24 13:31:11 +0000

    Uh 381 search results toggle headers (#154)
    * search fixes
    
    * rebased merged and unconflicted

--- dit_helpdesk/search/forms.py
+++ dit_helpdesk/search/forms.py
@@ -1,12 +1,14 @@
 from django import forms
 
-SORT_CHOICES = (("ranking", "Ranking"), ("commodity_code", "Code"))
+SORT_CHOICES = (("ranking", "Relevance"), ("commodity_code", "Code"))
+TOGGLE_CHOICES = ((0, "All Results"), (1, "Complete commodity codes only"))
+SORT_ORDER = (("desc", "Descending"), ("asc", "Ascending"))
 
 
 class CommoditySearchForm(forms.Form):
     def __init__(self, *args, **kwargs):
-
         super(CommoditySearchForm, self).__init__(*args, **kwargs)
+
         self.fields["q"].widget.attrs["id"] = "search"
         self.fields["q"].widget.attrs["class"] = "govuk-input govuk-input--width-20"
 
@@ -25,10 +27,16 @@ class CommoditySearchForm(forms.Form):
         self.fields["sort"].widget.attrs["id"] = "sort"
         self.fields["q"].widget.attrs["class"] = "govuk-select"
 
+        self.fields["sort"].initial = "ranking"
+        self.fields["sort_order"].initial = "desc"
+        self.fields["toggle_headings"].initial = 0
+        self.fields["page"].initial = 1
+
     q = forms.CharField()
-    toggle_headings = forms.BooleanField(initial=1, required=False)
-    sort = forms.ChoiceField(choices=SORT_CHOICES, initial="ranking", required=False)
-    page = forms.CharField(
-        required=False, max_length=3, initial="1", widget=forms.HiddenInput()
+    toggle_headings = forms.ChoiceField(
+        choices=TOGGLE_CHOICES, widget=forms.HiddenInput
     )
-    country = forms.CharField(required=True, max_length=2, widget=forms.HiddenInput())
+    sort = forms.ChoiceField(choices=SORT_CHOICES, widget=forms.HiddenInput)
+    sort_order = forms.ChoiceField(choices=SORT_ORDER, widget=forms.HiddenInput)
+    page = forms.CharField(widget=forms.HiddenInput())
+    country = forms.CharField(widget=forms.HiddenInput())
--- dit_helpdesk/search/helpers.py
+++ dit_helpdesk/search/helpers.py
@@ -1,6 +1,5 @@
 import logging
 import json
-import re
 
 from django_elasticsearch_dsl.search import Search
 from elasticsearch import Elasticsearch
@@ -12,55 +11,63 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-def search_by_term(
-    query, page, sort_by="ranking", sort_order="desc", filter_on_leaf=False
-):
+def search_by_term(form_data=None, page=None, query=None):
+
     client = Elasticsearch(hosts=[settings.ES_URL])
 
-    sort_object = {sort_by: {"order": sort_order}}
+    sort_object = {form_data.get("sort"): {"order": form_data.get("sort_order")}}
+
     query_object = {
         "multi_match": {
-            "query": query,
+            "query": form_data.get("q"),
             "type": "most_fields",
             "fields": ["keywords", "description"],
-            "operator": "and" if "," not in query else "or",
+            "operator": "and" if "," not in form_data.get("q") else "or",
         }
     }
 
-    request = (
-        Search()
-        .using(client)
-        .query(query_object)
-        .sort(sort_object)
-        .filter("term", leaf=filter_on_leaf)
-    )
+    filter_on_leaf = True if form_data.get("toggle_headings") == "1" else False
 
-    start = (page - 1) * settings.RESULTS_PER_PAGE
+    if filter_on_leaf:
+        request = (
+            Search()
+            .using(client)
+            .query(query_object)
+            .sort(sort_object)
+            .filter("term", leaf=filter_on_leaf)
+        )
+    else:
+        request = Search().using(client).query(query_object).sort(sort_object)
+
+    start = (int(form_data.get("page")) - 1) * settings.RESULTS_PER_PAGE
     end = start + settings.RESULTS_PER_PAGE
-
     total_results = len(list(request.scan()))
     total_full_pages = int(total_results / settings.RESULTS_PER_PAGE)
     orphan_results = total_results % settings.RESULTS_PER_PAGE
-
     total = request.count()
-
     request = request[0:total]
+
     hits = request[start:end].execute()
-    hits = remove_duplicate_commodity_hits(hits)
+
     for hit in hits:
         try:
             hit["hierarchy_context"] = json.loads(hit["hierarchy_context"])
         except KeyError as exception:
             logger.info("{0} {1}".format(hit["commodity_code"], exception.args))
 
+    page_range_start = start if start != 0 else start + 1
+    page_range_end = (
+        end if len(hits) == settings.RESULTS_PER_PAGE else start + len(hits)
+    )
+    total_pages = total_full_pages + 1 if orphan_results > 0 else total_full_pages
+
     return {
         "results": hits,
-        "page_range_start": start if start != 0 else start + 1,
-        "page_range_end": end
-        if len(hits) == settings.RESULTS_PER_PAGE
-        else start + len(hits),
-        "total_pages": total_full_pages + 1 if orphan_results > 0 else total_full_pages,
+        "page_range_start": page_range_start,
+        "page_range_end": page_range_end,
+        "total_pages": total_pages,
         "total_results": total_results,
+        "no_results": True if not total_results else False,
     }
 
 
@@ -69,25 +76,12 @@ def search_by_code(code):
     query_object = {"term": {"commodity_code": processed_query}}
     client = Elasticsearch(hosts=[settings.ES_URL])
     hits = Search().using(client).query(query_object)
-    hits = remove_duplicate_commodity_hits(hits)
     for hit in hits:
         try:
             hit["hierarchy_context"] = json.loads(hit["hierarchy_context"])
         except KeyError as exception:
             logger.info("{0} {1}".format(hit["commodity_code"], exception.args))
     return hits
-
-
-def remove_duplicate_commodity_hits(response):
-    # filter out duplicate headings and subheadings from the results
-    seen = set()
-    results = []
-    for hit in response:
-        key = str(hit["commodity_code"]) + hit["description"]
-        if key not in seen:
-            results.append(hit)
-            seen.add(key)
-    return results
 
 
 def process_commodity_code(code):
