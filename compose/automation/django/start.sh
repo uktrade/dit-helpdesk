c5329c90f61536fa3f2062bdb08f5fb69ba4e7d8
Pippo Raimondi <filipporai@gmail.com>
2020-01-29 16:50:04 +0000

    Use static file instead of preparing data

--- compose/automation/django/start.sh
+++ compose/automation/django/start.sh
@@ -24,11 +24,11 @@ python manage.py collectstatic --noinput
 python manage.py migrate
 python manage.py loaddata countries_data
 python manage.py use_static_api_data
-python manage.py prepare_import_data
+python manage.py prepare_static_import_data
 python manage.py scrape_section_hierarchy
 python manage.py import_rules_of_origin --data_path "import"
 python manage.py import_regulations
-python manage.py import_search_keywords -f output/commodity_category_all_with_synonyms_greenpage.csv
+python manage.py import_search_keywords -f output/commodity_category_all_with_synonyms_greenpage_subset.csv
 python manage.py search_index --populate
 # -------------------------------------------
 
