243360e9306cec6ed381db32828b23274785c079
Pippo Raimondi <filipporai@gmail.com>
2020-01-30 11:00:32 +0000

    Update tests and readme with new instructions on how to execute tests

--- ui_test/specs/test_search_country_and_commodity.py
+++ ui_test/specs/test_search_country_and_commodity.py
@@ -6,13 +6,13 @@ from ui_test.user_flows import search_country, search_commodity
 def test_search_country_and_commodity_by_keyword(browser):
     search_country(browser, 'Brazil')
     search_commodity(browser, 'Coffee')
-    assert(browser.is_text_present('Showing 13 results for Coffee'))
+    assert(browser.is_text_present('Showing 3 results for Coffee'))
 
 
 def test_search_country_and_commodity_by_code(browser):
     search_country(browser, 'Brazil')
-    search_commodity(browser, '2101129800')
-    assert(browser.is_text_present('Export 2101.12.98 from Brazil to the United Kingdom'))
+    search_commodity(browser, '2101000000')
+    assert(browser.is_text_present('Export 2101.00.00.00 from Brazil to the United Kingdom'))
 
 def test_empty_country(browser):
     search_country(browser, '')
