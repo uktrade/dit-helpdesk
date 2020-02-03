d6061d2c4ad63d754b04ac63bd7514d3bde44b33
Pippo Raimondi <filipporai@gmail.com>
2020-02-03 12:55:09 +0000

    Add no sandbox option to chrome

--- conftest.py
+++ conftest.py
@@ -1,14 +1,19 @@
 import pytest
 import ui_test.config as config
+
+from selenium.webdriver import ChromeOptions
 from splinter import Browser
 
+chrome_options = ChromeOptions()
+chrome_options.add_argument('--no-sandbox')
 
 @pytest.fixture(scope='module')
 def browser():
-    browser = Browser('chrome')
+    browser = Browser('chrome', headless=True, options=chrome_options)
     yield browser
     print('teardown')
     browser.quit()
+
 
 @pytest.fixture(autouse=True)
 def fixture_func(browser):
