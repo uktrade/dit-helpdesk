645d5043daf9d820174ced16aee3339ad6558dd3
Pippo Raimondi <filipporai@gmail.com>
2020-02-05 13:17:16 +0000

    Ensure folder is created before moving test fixtures

--- dit_helpdesk/trade_tariff_service/management/commands/use_static_api_data.py
+++ dit_helpdesk/trade_tariff_service/management/commands/use_static_api_data.py
@@ -3,12 +3,14 @@ import os
 from django.conf import settings
 from django.core.management.base import BaseCommand
 from shutil import copy
+from ...utils import createDir
 
 STATIC_TEST_FIXTURES = settings.IMPORT_DATA_PATH.format('test')
 DATA_SOURCE = settings.IMPORT_DATA_PATH.format('downloaded')
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        createDir(DATA_SOURCE)
         files = os.listdir(STATIC_TEST_FIXTURES)
         for f in files:
             copy("{dir_name}/{file_name}".format(dir_name=STATIC_TEST_FIXTURES, file_name=f), DATA_SOURCE)
