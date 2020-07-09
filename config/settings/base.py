4e4fa4f6850944f4e604df10196a561c867ccf07
trbateman <t.r.bateman85@gmail.com>
2020-07-09 19:03:06 +0100

    Changed quotes

--- config/settings/base.py
+++ config/settings/base.py
@@ -29,7 +29,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'elasticapm.contrib.django',
+    "elasticapm.contrib.django",
     "formtools",
     "core",
     "commodities",
