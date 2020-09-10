484f3e813ddbe1d7d34d4e17cbc47ad0b08d2509
Kevin Carrogan <kevin.carrogan@gmail.com>
2020-09-10 16:24:40 +0100

    Add management command to promote and de-dupe regulations

---
+++ dit_helpdesk/regulations/management/commands/promote_regulations.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+
+from hierarchy.models import Section
+from regulations.hierarchy import promote_regulations
+
+
+class Command(BaseCommand):
+    help = (
+        """Command to promote and dedupe regulations"""
+    )
+
+    def handle(self, *args, **options):
+        self.stdout.write("Promoting and de-duping regulations")
+        for section in Section.objects.all():
+            self.stdout.write(f"Promoting and de-duping for {section}")
+            promote_regulations(section)
