f0f486e3c1cd9bafd597c5da16dd09e8c14178dd
Kevin Carrogan <kevin.carrogan@gmail.com>
2020-09-25 15:12:18 +0100

    Add stub for cms regulation list
    This sets up some of the basic setup for the CMS:
    
      - Base styles
      - Gulp integration to build styles
      - Base template for CMS

--- dit_helpdesk/cms/urls.py
+++ dit_helpdesk/cms/urls.py
@@ -7,4 +7,5 @@ app_name = "cms"
 
 urlpatterns = [
     path("", views.CMSView.as_view(), name="home"),
+    path("regulation-groups/", views.RegulationGroupsListView.as_view(), name="regulation-groups-list"),
 ]
