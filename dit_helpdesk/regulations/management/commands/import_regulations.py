cabc26036a4ca99657bca3d56ff62f1c81f6d17b
Andrzej Lichaczewski <andrzej.lichaczewski@gmail.com>
2020-10-08 11:21:54 +0200

    Binding NomenclatureTree to Regulation and RegulationGroup objects

--- dit_helpdesk/regulations/management/commands/import_regulations.py
+++ dit_helpdesk/regulations/management/commands/import_regulations.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.db import transaction
 from django.core.management.base import BaseCommand
 
 from hierarchy.models import Section
@@ -13,16 +14,25 @@ class Command(BaseCommand):
         """Command to import links to documents and regulations related to commodties"""
     )
 
+    def import_regulations(self, region, data_path):
+        self.stdout.write("Importing regulations")
+        importer = RegulationsImporter(region=region)
+        importer.load(data_path)
+        importer.process()
+
+        self.stdout.write("Promoting and de-duping regulations")
+        for section in Section.objects.all():
+            self.stdout.write(f"Promoting and de-duping for {section}")
+            promote_regulation_groups(section)
+
     def handle(self, *args, **options):
         data_path = settings.REGULATIONS_DATA_PATH
 
-        with process_swapped_tree():
-            self.stdout.write("Importing regulations")
-            importer = RegulationsImporter()
-            importer.load(data_path)
-            importer.process()
+        with transaction.atomic():
+            with process_swapped_tree():
+                # using `process_swapped_tree` on the default region, as that tree is updated
+                # in the background - this tree should remain inactive until it's fully populated
+                self.import_regulations('EU', data_path)
 
-            self.stdout.write("Promoting and de-duping regulations")
-            for section in Section.objects.all():
-                self.stdout.write(f"Promoting and de-duping for {section}")
-                promote_regulation_groups(section)
+            # we don't care about this tree being partially updated for a few minutes for now
+            self.import_regulations('UK', data_path)
