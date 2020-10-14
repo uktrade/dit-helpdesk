31ffb549bda0a2cb8fe16505cc088b0c33602003
Andrzej Lichaczewski <andrzej.lichaczewski@gmail.com>
2020-10-14 14:48:36 +0200

    Reverted files

--- dit_helpdesk/search/data/output/compare.py
+++ dit_helpdesk/search/data/output/compare.py
@@ -1,15 +1,9 @@
-def main():
+with open("commodity_category_all_with_synonyms_greenpage.csv", "r") as file1:
+    with open("keywords_and_synonyms_merged.csv", "r") as file2:
+        same = set(file1).difference(file2)
 
-    with open("commodity_category_all_with_synonyms_greenpage.csv", "r") as file1:
-        with open("keywords_and_synonyms_merged.csv", "r") as file2:
-            same = set(file1).difference(file2)
+same.discard("\n")
 
-    same.discard("\n")
-
-    with open("some_output_file.txt", "w") as file_out:
-        for line in same:
-            file_out.write(line)
-
-
-if __name__ == '__main__':
-    main()
+with open("some_output_file.txt", "w") as file_out:
+    for line in same:
+        file_out.write(line)
