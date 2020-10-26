bcb53e636a8b0d1767242cb8cd796cc7d8008fa0
Kevin Carrogan <kevin.carrogan@gmail.com>
2020-10-26 13:29:50 +0000

    Handle accented characters in search

--- dit_helpdesk/search/documents/util.py
+++ dit_helpdesk/search/documents/util.py
@@ -3,6 +3,6 @@ from elasticsearch_dsl import analyzer
 html_strip = analyzer(
     "html_strip",
     tokenizer="standard",
-    filter=["standard", "lowercase", "stop", "snowball"],
+    filter=["standard", "lowercase", "stop", "snowball", "asciifolding"],
     char_filter=["html_strip"],
 )
