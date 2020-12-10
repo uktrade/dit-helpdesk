a0ac3ed06d3711b74da5b07d93aab8be6505720d
Andrzej Lichaczewski <andrzej.lichaczewski@gmail.com>
2020-12-10 19:20:23 +0100

    Importing new RoO files from S3

--- dit_helpdesk/rules_of_origin/management/commands/import_rules_of_origin.py
+++ dit_helpdesk/rules_of_origin/management/commands/import_rules_of_origin.py
@@ -1,12 +1,14 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
+from tempfile import NamedTemporaryFile
 
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
 from rules_of_origin.models import Rule, SubRule, RulesDocument, RulesDocumentFootnote
 from rules_of_origin.ingest.importer import import_roo
+from rules_of_origin.ingest.s3 import _get_s3_bucket
 
 from hierarchy.helpers import process_swapped_tree
 
@@ -19,14 +21,7 @@ class Command(BaseCommand):
         with process_swapped_tree():
             self._handle(*args, **options)
 
-    def _handle(self, *args, **options):
-
-        path = settings.RULES_OF_ORIGIN_DATA_PATH
-
-        if options["reset"]:
-            for cls in Rule, SubRule, RulesDocument, RulesDocumentFootnote:
-                cls.objects.all().delete()
-
+    def _import_local(self, path):
         if os.path.isdir(path):
 
             for root, dirs, files in os.walk(path):
@@ -38,3 +33,38 @@ class Command(BaseCommand):
 
         else:
             import_roo(path)
+
+    def _import_from_s3(self):
+        bucket = _get_s3_bucket(
+            bucket_name=settings.ROO_S3_BUCKET_NAME,
+            access_key_id=settings.ROO_S3_ACCESS_KEY_ID,
+            access_key=settings.ROO_S3_SECRET_ACCESS_KEY,
+        )
+
+        summaries = bucket.objects.filter(Prefix='rules_of_origin/')
+        xml_objects = [s.Object() for s in summaries if s.key.endswith('.xml')]
+
+        for obj in xml_objects:
+            with NamedTemporaryFile(mode='w+b') as temp_file:
+                self.stdout.write(f"Attempting to download object {obj.key} from S3")
+                obj.download_fileobj(temp_file)
+                self.stdout.write(f"Downloaded {obj.key}..")
+
+                temp_file.flush()
+                temp_file.seek(0)
+
+                import_roo(temp_file)
+
+    def _handle(self, *args, **options):
+
+        local_path = settings.RULES_OF_ORIGIN_DATA_PATH
+        s3_bucket = settings.ROO_S3_BUCKET_NAME
+
+        if options["reset"]:
+            for cls in Rule, SubRule, RulesDocument, RulesDocumentFootnote:
+                cls.objects.all().delete()
+
+        if not s3_bucket and local_path:
+            self._import_local(local_path)
+        else:
+            self._import_from_s3()
