d5a414cbe0ed18540349ef46922aa0b8fb4511dd
Andrzej Lichaczewski <andrzej.lichaczewski@gmail.com>
2020-12-17 12:12:18 +0100

    Management command for flags

--- dit_helpdesk/core/management/commands/manage_flags.py
+++ dit_helpdesk/core/management/commands/manage_flags.py
@@ -3,15 +3,39 @@
 import os
 
 from django.conf import settings
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 
 from flags.models import FlagState
 
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        parser.add_argument("--list", action="store_true")
-        parser.add_argument("--set_bool", action="store_true")
+        parser.add_argument("-l", "--list", action="store_true", help="List flags")
+        parser.add_argument(
+            "-s", "--set", nargs="*", action="store",
+            help="Set a flag value. Usage: --set flag_name flag_value")
+
+    def list_flags(self):
+        flags = FlagState.objects.order_by('name').all()
+
+        for flag in flags:
+            self.stdout.write(
+                f"Name: {flag.name}\t Condition: {flag.condition}\t Value: {flag.value}")
+
+    def set_flag(self, name, value):
+        flag = FlagState.objects.get(
+            name=name)
+
+        flag.value = value
+        flag.save()
 
     def handle(self, *args, **options):
-        pass
+        if options["list"]:
+            self.list_flags()
+        elif options["set"]:
+            try:
+                name, value = options["set"]
+            except ValueError as e:
+                raise CommandError("Provide 2 values for the set option") from e
+
+            self.set_flag(name, value)
