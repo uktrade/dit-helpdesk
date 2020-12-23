420e9a125d77c3ad035ecae11ea0dd7d0b16607c
Kevin Carrogan <kevin.carrogan@gmail.com>
2020-12-23 14:49:27 +0000

    Move trade tariff config into env settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -220,18 +220,7 @@ CONTACT_MAX_LENGTH = 1000
 # trade tariff service arguments
 IMPORT_DATA_PATH = APPS_DIR + "/trade_tariff_service/import_data/{0}"
 
-TRADE_TARIFF_CONFIG = {
-    "UK": {
-        "TREE": {
-            "BASE_URL": "https://www.trade-tariff.service.gov.uk/api/v2/",
-        },
-    },
-    "EU": {
-        "TREE": {
-            "BASE_URL": "https://www.trade-tariff.service.gov.uk/api/v2/",
-        },
-    },
-}
+TRADE_TARIFF_CONFIG = env.json("TRADE_TARIFF_CONFIG")
 
 # We don't have this at the section level due to not requiring any information from the trade tariff service at this level
 TTS_CHAPTER_URL = "https://www.trade-tariff.service.gov.uk/api/v1/chapters/{0}"
