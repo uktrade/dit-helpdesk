003aae9db218a1d61b90c6ad662694735cf9e2e0
Kevin Carrogan <kevin.carrogan@gmail.com>
2021-02-12 16:11:36 +0000

    Raise exception when deferred save form state is invalid

--- dit_helpdesk/deferred_save/models.py
+++ dit_helpdesk/deferred_save/models.py
@@ -1,6 +1,13 @@
-from django.db import models
 from django.contrib.postgres.fields import JSONField
+from django.db import models
 from django.utils.module_loading import import_string
+
+
+class InvalidDataError(Exception):
+
+    def __init__(self, form, *args, **kwargs):
+        self.form = form
+        super().__init__(*args, **kwargs)
 
 
 class DeferredSave(models.Model):
@@ -11,6 +18,9 @@ class DeferredSave(models.Model):
         form_class = import_string(self.form_class)
 
         form = form_class(self.data)
+        if not form.is_valid():
+            raise InvalidDataError(form)
+
         instance = form.save()
 
         return form, instance
