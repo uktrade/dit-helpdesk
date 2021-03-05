246c98c06e4a09cb31eed34204a1bf2a6e719c87
Andrzej Lichaczewski <andrzej.lichaczewski@gmail.com>
2021-03-05 17:01:07 +0100

    Exceptions hierarchy, moved footnote processor to separate module

--- dit_helpdesk/hierarchy/models.py
+++ dit_helpdesk/hierarchy/models.py
@@ -20,6 +20,8 @@ from rules_of_origin.models import (
     Rule,
     RulesDocumentFootnote,
 )
+from rules_of_origin.footnote_processor import FootnoteReferenceProcessor
+from rules_of_origin.exceptions import RulesOfOriginException
 from trade_tariff_service.tts_api import (
     ChapterJson,
     HeadingJson,
@@ -83,60 +85,6 @@ class TreeSelectorMixin:
             self.nomenclature_tree = NomenclatureTree.get_active_tree(settings.PRIMARY_REGION)
 
         super().save()
-
-
-class FootnoteReferenceProcessor:
-    """Used to keep state of encountered notes references in multiple rule texts."""
-
-    NOTE_REFERENCE_REGEX = re.compile(r"\[[A-Za-z0-9]+\]")
-    INTRODUCTORY_NOTE_REFERENCE_REGEX = re.compile(r"@{doc:COMM}\[([A-Za-z0-9\s]+)\]")
-
-    def __init__(self):
-        self.found_note_ids = []
-        self.unique_note_ids = set()
-        self.note_number_by_id = {}
-
-    def _replace_note_reference(self, ref_match):
-        ref = ref_match.group()
-        ref_id = ref.strip('[]')
-
-        self.found_note_ids.append(ref_id)
-        self.unique_note_ids.add(ref_id)
-
-        # each newly encountered note reference gets an n+1 number, but if we've seen it before
-        # we fetch the note reference number like it was when it was seen for the first time
-        # e.g. [A] [B] [C] [B] would result in numbering of: A=1, B=2, C=3, so after replacing:
-        # [1] [2] [3] [2]
-        if ref_id not in self.note_number_by_id:
-            self.note_number_by_id[ref_id] = len(self.unique_note_ids)
-
-        note_number = self.note_number_by_id[ref_id]
-
-        return f"<sup>{note_number})</sup>"
-
-    def replace_all_notes_references(self, text):
-        if not text:
-            return text
-
-        text_replaced = self.NOTE_REFERENCE_REGEX.sub(
-            self._replace_note_reference, text
-        )
-        return text_replaced
-
-    def _replace_introductory_note_reference(self, ref_match):
-        ref, *_ = ref_match.groups()
-
-        ref_replaced = f"{ref} (below)"
-        return ref_replaced
-
-    def replace_all_introductory_notes_references(self, text):
-        if not text:
-            return text
-
-        text_replaced = self.INTRODUCTORY_NOTE_REFERENCE_REGEX.sub(
-            self._replace_introductory_note_reference, text
-        )
-        return text_replaced
 
 
 class RulesOfOriginMixin:
@@ -263,7 +211,7 @@ class RulesOfOriginMixin:
                 identifier="COMM",
             )
         except RulesDocumentFootnote.DoesNotExist as e:
-            raise ValueError(f"Could not find introductory notes for {country}") from e
+            raise RulesOfOriginException(f"Could not find introductory notes for {country}") from e
 
         rules_of_origin = {
             "rules": rules,
