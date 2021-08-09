2cb305435d079d634f6b76947e38e8caa66f313b
Kevin Carrogan <kevin.carrogan@gmail.com>
2021-08-09 08:33:25 +0100

    Handle case where countries may use multiple country codes
    There is the case, for Serbia in particular, where internally we use one country code but externally the country is identified by an alternative country code e.g. https://www.gov.uk/guidance/change-to-the-eu-country-code-for-serbia-tariff-stop-press-notice-30

--- dit_helpdesk/countries/models.py
+++ dit_helpdesk/countries/models.py
@@ -2,8 +2,12 @@ from django.db import models
 
 
 class Country(models.Model):
-
+    # We have two country codes, one which represents the trading country code
+    # and the other an optional alternative original country code.
+    # e.g. https://www.gov.uk/guidance/change-to-the-eu-country-code-for-serbia-tariff-stop-press-notice-30
     country_code = models.CharField(max_length=2)  # ISO_3166-1_alpha-2 code
+    alternative_non_trade_country_code = models.CharField(max_length=2, null=True)
+
     name = models.CharField(max_length=250)
 
     scenario = models.CharField(max_length=255)
