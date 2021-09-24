f32951964dadec0d463a7da70a778d72e1629f03
santinomolinaro <santino.molinaro@mail>
2021-09-24 17:12:19 +0100

    Migrating new country schema for new scenario categories

--- dit_helpdesk/countries/models.py
+++ dit_helpdesk/countries/models.py
@@ -15,8 +15,15 @@ class Country(models.Model):
 
     has_uk_trade_agreement = models.BooleanField(default=False)
     has_eu_trade_agreement = models.BooleanField(default=False)
+    trade_agreement_title = models.CharField(max_length=250, null=True)
+    trade_agreement_type = models.CharField(max_length=250, null=True)
 
     is_eu = models.BooleanField(default=False)
+
+    # These 2 fields are temporary and will replace scenario and content_url fields above
+    # scenario needs to be null for now or existing entries will break
+    new_scenario = models.CharField(max_length=255, null=True)
+    new_trade_agreement_url = models.URLField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.country_code} - {self.name} - {self.scenario}"
