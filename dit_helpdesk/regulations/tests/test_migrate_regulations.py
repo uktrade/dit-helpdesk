5319214543b7a7217dbe1d296bb60fef8ad10fb7
Kevin Carrogan <kevin.carrogan@gmail.com>
2021-10-15 09:13:20 +0100

    Strip back migrate regulations tests

--- dit_helpdesk/regulations/tests/test_migrate_regulations.py
+++ dit_helpdesk/regulations/tests/test_migrate_regulations.py
@@ -8,26 +8,14 @@ from django.utils import timezone
 
 from commodities.models import Commodity
 from hierarchy.helpers import create_nomenclature_tree
-from hierarchy.models import Section, Chapter, Heading, SubHeading, NomenclatureTree
+from hierarchy.models import NomenclatureTree
 from regulations.models import RegulationGroup
 
 
 class MigrateRegulationsTest(TestCase):
     def setUp(self) -> None:
         self.tree = create_nomenclature_tree()
-        self.section = mixer.blend(Section, nomenclature_tree=self.tree)
-        self.chapter = mixer.blend(
-            Chapter, section=self.section, nomenclature_tree=self.tree
-        )
-        self.heading = mixer.blend(
-            Heading, chapter=self.chapter, nomenclature_tree=self.tree
-        )
-        self.subheading = mixer.blend(
-            SubHeading, heading=self.heading, nomenclature_tree=self.tree
-        )
-        self.commodity = mixer.blend(
-            Commodity, parent_subheading=self.subheading, nomenclature_tree=self.tree
-        )
+        self.commodity = mixer.blend(Commodity, nomenclature_tree=self.tree)
 
         self.new_tree = NomenclatureTree.objects.create(
             region="UK", start_date=timezone.now(), end_date=timezone.now()
@@ -39,7 +27,7 @@ class MigrateRegulationsTest(TestCase):
         )
 
     def test_migrate_regulation_groups(self):
-        regulation_group = mixer.blend(RegulationGroup, commodities=self.commodity)
+        regulation_group = mixer.blend(RegulationGroup)
         regulation_group.nomenclature_trees.add(self.tree)
         regulation_group.save()
 
@@ -48,7 +36,7 @@ class MigrateRegulationsTest(TestCase):
         self.assertIn(self.new_tree, regulation_group.nomenclature_trees.all())
         self.assertEqual(self.new_tree.regulationgroup_set.count(), 1)
 
-    def test_migrate_nomenclature_objects(self):
+    def test_migrate_nomenclature_object_regulation_groups(self):
         regulation_group = mixer.blend(RegulationGroup, commodities=self.commodity)
         regulation_group.nomenclature_trees.add(self.tree)
         regulation_group.save()
@@ -57,16 +45,3 @@ class MigrateRegulationsTest(TestCase):
 
         self.assertIn(regulation_group, self.new_commodity.regulationgroup_set.all())
         self.assertEqual(self.new_commodity.regulationgroup_set.count(), 1)
-
-    def test_migrate_nomenclature_objects__unbind_commodity(self):
-        regulation_group = mixer.blend(RegulationGroup, commodities=self.commodity)
-        regulation_group.nomenclature_trees.add(self.tree)
-        regulation_group.save()
-
-        self.commodity.regulationgroup_set.clear()
-        self.commodity.save()
-
-        call_command("migrate_regulations", stdout=sys.stdout)
-
-        self.assertNotIn(regulation_group, self.new_commodity.regulationgroup_set.all())
-        self.assertEqual(self.new_commodity.regulationgroup_set.count(), 0)
