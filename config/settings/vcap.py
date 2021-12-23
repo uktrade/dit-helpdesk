0da33c31a7e08d755a9c5d61de9cb7a1e89b821e
santinomolinaro <santino.molinaro@mail>
2021-12-23 14:13:24 +0000

    Changes to switch ElasticSearch to OpenSearch

--- config/settings/vcap.py
+++ config/settings/vcap.py
@@ -5,7 +5,7 @@ from .env import env
 
 VCAP_SERVICES = json.loads(env.str("VCAP_SERVICES"))
 
-ES_URL = VCAP_SERVICES["elasticsearch"][0]["credentials"]["uri"]
+ES_URL = VCAP_SERVICES["opensearch"][0]["credentials"]["uri"]
 REDIS_URL = VCAP_SERVICES["redis"][0]["credentials"]["uri"]
 
 try:
