f14a3f99c43f628e7e62457730f7471aa45b2b13
Shehzad Ali <shehzadashiq@hotmail.com>
2023-03-28 11:04:13 +0100

    Setting PG/Redis environment variables
    Signed-off-by: Shehzad Ali <shehzadashiq@hotmail.com>

--- config/settings/vcap.py
+++ config/settings/vcap.py
@@ -5,7 +5,34 @@ from .env import env
 VCAP_SERVICES = json.loads(env.str("VCAP_SERVICES"))
 
 ES_URL = VCAP_SERVICES["opensearch"][0]["credentials"]["uri"]
-REDIS_URL = VCAP_SERVICES["redis"][0]["credentials"]["uri"]
+
+
+# Copilot env key.  IF set, we turn the supplied json into a DB connection string
+# that can be consumed by dj-database-url
+DATABASE_CREDS = env.json("DATABASE_CREDENTIALS", default={})
+
+if DATABASE_CREDS:
+    db_url = "{engine}://{username}:{password}@{host}:{port}/{dbname}".format(**DATABASE_CREDS)
+    os.environ["DATABASE_URL"] = db_url
+    
+# Redis
+if "redis" in VCAP_SERVICES:
+    REDIS_URL = VCAP_SERVICES["redis"][0]["credentials"]["uri"]
+else:
+    REDIS_URL = env("REDIS_ENDPOINT", default=None)
+
+# COPILOT configuration
+# if not CELERY_BROKER_URL:
+#     CELERY_BROKER_URL = env("REDIS_ENDPOINT")
+
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django_redis.cache.RedisCache",
+#         "LOCATION": CELERY_BROKER_URL,
+#         "OPTIONS": {"CLIENT_CLASS": "django_redis.client.DefaultClient"},
+#         "KEY_PREFIX": "wp_",
+#     }
+# }
 
 DATABASES = {"default": env.db()}
 
