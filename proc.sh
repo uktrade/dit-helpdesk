d323a9c26b2a66f83e2d32a22b801c842c911992
Jayesh Patel <jayesh.patel@digital.trade.gov.uk>
2023-04-12 17:41:18 +0100

    fix for workers

--- proc.sh
+++ proc.sh
@@ -12,5 +12,5 @@ if [ $? -ne 0 ]; then
     exit
 fi
 
-# gunicorn -c config/gunicorn.py config.wsgi:application --worker-class=gevent --worker-connections=1000 --workers 9 --bind 0.0.0.0:$PORT
-gunicorn -c config/gunicorn.py config.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn -c config/gunicorn.py config.wsgi:application --worker-class=gevent --worker-connections=1000 --workers 9 --timeout 0 --bind 0.0.0.0:$PORT
+# gunicorn -c config/gunicorn.py config.wsgi:application --bind 0.0.0.0:$PORT
